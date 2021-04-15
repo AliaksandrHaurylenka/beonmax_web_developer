3f3e92bfd65a72777b4a33efdede24114950fb8c
Aliaksandr <goric0312@mail.ru>
2021-04-15 13:07:10 +0300

    asd

--- web-developer-gulp-bem/src/mailer/smart.php
+++ web-developer-gulp-bem/src/mailer/smart.php
@@ -10,35 +10,38 @@ $mail->CharSet = 'utf-8';
 
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mailtrap.io';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'd47437d27d54e1';                 // Наш логин
-$mail->Password = '520a54484aa749';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 2525;                                   // TCP port to connect to
- 
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('goric0312@mail.ru');     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Данные';
-$mail->Body    = '
-		Пользователь оставил данные <br> 
-	Имя: ' . $name . ' <br>
-	Номер телефона: ' . $phone . '<br>
-	E-mail: ' . $email . '';
-
-if(!$mail->send()) {
-    return false;
-} else {
-    return true;
+if ($name != "" and $phone != "" and $email != ""){
+	$mail->isSMTP();                                      // Set mailer to use SMTP
+	$mail->Host = 'smtp.mailtrap.io';  // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;                               // Enable SMTP authentication
+	$mail->Username = 'd47437d27d54e1';                 // Наш логин
+	$mail->Password = '520a54484aa749';                           // Наш пароль от ящика
+	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 2525;                                   // TCP port to connect to
+	 
+	$mail->setFrom('', 'Pulse');   // От кого письмо 
+	$mail->addAddress('goric0312@mail.ru');     // Add a recipient
+	//$mail->addAddress('ellen@example.com');               // Name is optional
+	//$mail->addReplyTo('info@example.com', 'Information');
+	//$mail->addCC('cc@example.com');
+	//$mail->addBCC('bcc@example.com');
+	//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+	//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+	$mail->isHTML(true);                                  // Set email format to HTML
+	
+	$mail->Subject = 'Данные';
+	$mail->Body    = '
+			Пользователь оставил данные <br> 
+		Имя: ' . $name . ' <br>
+		Номер телефона: ' . $phone . '<br>
+		E-mail: ' . $email . '';
+	
+	if(!$mail->send()) {
+		return false;
+	} else {
+		return true;
+	}
 }
+
 
 ?>
