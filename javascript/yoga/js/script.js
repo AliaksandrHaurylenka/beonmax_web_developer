8e433204b7897816cb124833967646bd57f0d85f
Aliaksandr <goric0312@mail.ru>
2021-06-09 10:57:29 +0300

    asd

--- javascript/yoga/js/script.js
+++ javascript/yoga/js/script.js
@@ -53,4 +53,101 @@ window.addEventListener('DOMContentLoaded', function() {
 
     });
     //КОНЕЦ ПЕРЕКЛЮЧЕНИЕ ТАБОВ
+
+
+    // Timer 
+
+    let deadline = '2021-06-10';//время до которого будет идти таймер
+
+    /**
+     * Функция возвращающая секунды
+     * минуты и часы
+     * @param {string} endtime 
+     */
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),//разница между датой до которой должен быть отсчет и текущей в миллисекундах
+            seconds = Math.floor((t/1000) % 60),// получаем секунды округленные до целого числа и берется остаток от деления на 60
+            minutes = Math.floor((t/1000/60) % 60),//получаем минуты округленные до целого числа и берется остаток от деления на 60
+            hours = Math.floor((t/(1000*60*60)));//получаем часы округленные до целого числа
+            //hours = Math.floor((t/(1000/60/60) % 24)),//получаем часы округленные до целого числа
+            //days = Math.floor((t/(1000*60*60*24)));//получаем дни округленные до целого числа
+
+        return {//возвращаем объект
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds,
+            //'days' : days
+        };
+    }
+
+    /**
+     * Функция запускающая таймер
+     * @param {string} id 
+     * @param {string} endtime 
+     */
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            //days = timer.querySelector('.days'),
+            timeInterval = setInterval(updateClock, 1000);//обновление счетчика каждую секунду
+            
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num){//добавляем 0 к единичным цифрам
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    };
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+            //days.textContent = addZero(t.days);
+
+            if (t.total <= 0) {//если t = 0 останавливаем счетчик
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+                //days.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
+
+
+    //Modal
+    let more = document.querySelector('.more'),
+        descriptionBtn = document.querySelector('.description-btn'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+        // more.addEventListener('click', function(){
+        //     overlay.style.display = 'block';
+        //     this.classList.add('more-splash');
+        //     document.body.style.overflow = 'hidden';
+        // });
+
+        function overlayBlock(btn){
+            btn.addEventListener('click', function(){
+                overlay.style.display = 'block';
+                this.classList.add('more-splash');
+                document.body.style.overflow = 'hidden';
+            });
+        }
+
+        overlayBlock(more);
+        overlayBlock(descriptionBtn);
+
+        close.addEventListener('click', function(){
+            overlay.style.display = 'none';
+            more.classList.remove('more-splash');
+            document.body.style.overflow = '';
+        });
 });
